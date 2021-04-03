112fb6ded7a4cf0ffe61a2cf0ffee329f900e688
Jonas Alves <jonasalves@alu.ufc.br>
2021-04-03 14:04:58 -0300

    Início do desenvolvimento das configurações dos usuários
    Foi implementado um PopupMenuButton para selecionar as ações
    "Configurações" e "Sair". A primeira ação inicializa a tela
    ConsumerConfigurationsScreen e a segunda faz o logout do usuário
    redirecionando ele para a tela de login.

---
+++ lib/screens/consumer/configurations.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:local_market/screens/authentication/index.dart';
+import 'package:provider/provider.dart';
+import 'package:local_market/utils/authentication.dart';
+
+class ConsumerConfigurationsScreen extends StatefulWidget {
+  @override
+  _ConsumerConfigurationsScreenState createState() =>
+      _ConsumerConfigurationsScreenState();
+}
+
+class _ConsumerConfigurationsScreenState
+    extends State<ConsumerConfigurationsScreen> {
+  Authentication auth;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    auth = Provider.of<Authentication>(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Configurações Usuário"),
+      ),
+      body: Container(),
+    );
+  }
+}
